65488cc546aa4a7a54d1d11374ab80a050c424f1
Victor Tasca <105996897+Victortascaa@users.noreply.github.com>
2023-12-06 13:30:28 -0300

    AULA11

---
+++ AULA11/script.js
@@ -0,0 +1,9 @@
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite um número');
+
+var numeroFormatado1 = parseInt(num1, 10);
+var numeroFormatado2 = parseInt(num2, 10);
+
+const somaTotal = (numeroFormatado1 + numeroFormatado2);
+
+alert('O total dessa soma é:' + somaTotal);
