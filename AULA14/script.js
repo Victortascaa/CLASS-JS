0351fb10f67b4ec164464dbff1931efaf31eee2d
Victor Tasca <105996897+Victortascaa@users.noreply.github.com>
2023-12-11 22:48:27 -0300

    teste

---
+++ AULA14/script.js
@@ -0,0 +1,10 @@
+let num1 = 10;
+let num2 = 2.5;
+
+// console.log(num1.toString()+num2);
+// num1= num1.toString();
+// console.log(num2.toString(2)); Converte em String (Texto)
+// console.log(num1.toFixed(2)); Arredondar número
+// console.log(Number.isInteger(num1)); Ver número inteiro
+let temp = num1 * 'Ola';
+console.log(Number.isNaN(temp))
